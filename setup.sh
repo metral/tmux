10f80243218f174cc043c9f83b036d1404894273
Mike Metral <metral@gmail.com>
2016-02-04 12:12:02 -0800

    upgrade to v2.1

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install libevent-dev libncurses-dev pkg-config -y
+
+wget https://github.com/tmux/tmux/releases/download/2.1/tmux-2.1.tar.gz -O /tmp/tmux.tar.gz
+pushd /tmp
+tar xvfz tmux*.tar.gz
+pushd tmux-*
+./configure && make
+sudo make install
+popd
+popd
+
+rm -rf /tmp/tmux*
